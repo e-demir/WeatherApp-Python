21049c0d12af76a341db86fe95b628a35b9ab6ef
Emrullah DEMİR <e177demir@gmail.com>
2021-12-24 22:44:53 +0300

    apikey

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import json
 
 while True:
     city = input("Enter a city and learn the weather:  ")
-    apiKey = "e2c9d6beb6f8c0f0f4ac901d63f2f4ab"
+    apiKey = "xxxapikeyxxx"
     address = "http://api.openweathermap.org/data/2.5/weather?q={}&appid={}&lang=tr&units=metric".format(city, apiKey)
     connect = requests.get(address)
     query = connect.json()
